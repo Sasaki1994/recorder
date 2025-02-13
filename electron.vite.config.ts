1740023e1eb096c5e6bc938fc240ce7e144201ca
Sasaki1994 <ssktty1994@gmail.com>
2025-02-14 02:02:17 +0900

    build後のソフトウェアでサブウィンドウが表示されない問題を修正

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { join } from 'path'
 
 export default defineConfig({
   main: {
@@ -13,6 +14,14 @@ export default defineConfig({
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          index: join(__dirname, 'src/renderer/index.html'),
+          timeshift: join(__dirname, 'src/renderer/timeshift.html')
+        }
       }
     },
     plugins: [react()]
